aaf6372ba92684f35b2a66503fd486245fbaed71
Antonio Rubio <anruca92@gmail.com>
2020-10-08 11:57:58 +0200

    primera subida

---
+++ environments/testing/main.tf
@@ -0,0 +1,10 @@
+
+module "webserver" {
+  source = "github.com/AntonioRubioCalzado/terraform_module_test?ref=v1.0.4"
+  vpc_id = "vpc-f030fd89"
+  project_name = "miproyecto"
+  environment = "testing"
+  key_name = "terraform"
+  ami_id = "ami-06fd8a495a537da8b"
+  instance_type = "t2.micro"
+}
